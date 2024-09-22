41cd9713a27b63b7dfa0c1d420fcfde5dca310dd
jinhyeon <tiemo0708@naver.com>
2024-09-22 18:12:33 +0900

    feat: 프로젝트생성, h2데이터베이스 연결

---
+++ src/main/java/project/DocumentAutomation/DocumentAutomationApplication.java
@@ -0,0 +1,13 @@
+package project.DocumentAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/DocumentAutomation/DocumentAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package project.DocumentAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
